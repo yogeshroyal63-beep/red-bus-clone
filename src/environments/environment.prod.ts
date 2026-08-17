789c51da731f6ed4d4729154eb0a284de5b7d78f
yogeshroyal63-beep <yogeshroyal63@gmail.com>
2026-08-17 11:11:04 +0530

    changes the url ised in enviorment.prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://your-api-domain.com/api',
+  apiUrl: 'https://red-bus-clone-q711.onrender.com/api',
   appName: 'redBus Clone',
   version: '2.0.0'
 };
